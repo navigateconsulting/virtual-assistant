db332a7919ea005acac6b31c818e95bc23af9f84
saikiran2603 <sai.kiran@navigateconsulting.in>
2019-04-16 19:21:23 +0530

    Modified Ui-Trainer app as per the latest changes , Added Mongodb Docker image Data folder mounted on the local system

---
+++ api-backend/run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+
+## To Execute the App use - FLASK_APP=run.py FLASK_DEBUG=1 flask run
+
+
+import views, models, resources
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
