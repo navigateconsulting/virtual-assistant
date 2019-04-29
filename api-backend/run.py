a1084dacd44b24c1db0d128528f3a9c351971214
sai <sai.kiran@navigateconsulting.in>
2019-04-29 23:31:47 +0530

    added Projects API , for Update and Query of projects

--- api-backend/run.py
+++ api-backend/run.py
@@ -41,4 +41,4 @@ api.add_resource(resources.UpdateProjects, '/updateprojects')
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8081)
