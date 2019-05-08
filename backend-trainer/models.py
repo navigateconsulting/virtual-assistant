a77dba91ff4e8c544beefd1a564e71c550311676
sai <sai.kiran@navigateconsulting.in>
2019-05-08 19:27:32 +0530

    refactoring get projects and new methods for domains

--- backend-trainer/models.py
+++ backend-trainer/models.py
@@ -21,6 +21,7 @@ class projectsModel():
     async def getProjects():
         cursor= db.projects.find()
         result=await cursor.to_list(length=1000)
+        print("Projects sent {}".format(json.loads(dumps(result))))
         return json.loads(dumps(result))
 
     async def createProjects(record):
@@ -31,14 +32,34 @@ class projectsModel():
     async def deleteProject(object_id):
         query = {"_id": ObjectId("{}".format(object_id))}
         result=db.projects.delete_one(query)
-        print("Project Deleted count {}".format(result.deleted_count))
-        return result.deleted_count
+        print("Project Deleted count {}".format(result))
+        return result
 
     async def updateProject(record):
-        query = {"_id": ObjectId("{}".format(record['object_id']))}
-        update_field = {"$set": {"project_id": record['project_id'], "project_name": record['project_name'],
-                                 "project_description": record['project_description']}}
-        result = db.projects.update_one(query,update_field)
-        print("Project Updated , rows modified {}".format(result.modified_count))
-        return result.modified_count
 
+        json_record = json.loads(record)
+
+        query = {"_id": ObjectId("{}".format(json_record['object_id']))}
+        update_field = {"$set": {"project_id": json_record['project_id'], "project_name": json_record['project_name'],
+                                 "project_description": json_record['project_description']}}
+        result = db.projects.update_one(query,update_field)
+        print("Project Updated , rows modified {}".format(result))
+        return result
+
+
+class domainsModel():
+
+    async def getDomains(project_id):
+        query = {"project_id": project_id}
+        cursor = db.domains.find(query)
+        result = await cursor.to_list(length=1000)
+        print("Domains sent {}".format(json.loads(dumps(result))))
+        return json.loads(dumps(result))
+
+    async def createDomain(data):
+
+        record = {"project_id":data['project_id'],"domain_id":data['domain_id'],"domain_name":data['domain_name'],"domain_description":data['domain_description'],
+                  "domain":{"Intents":[{}],"Stories":[{}],"Responses":[{}]}}
+        result = db.domains.insert_one(json.loads(record))
+        print("Domain created with ID {}".format(result.inserted_id))
+        return result.inserted_id
