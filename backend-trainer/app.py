646ffd3109dbcecd2a7cd37310ef0ce55a551d06
sai <sai.kiran@navigateconsulting.in>
2019-05-08 19:26:38 +0530

    refactoring projects and get Domains methods

--- backend-trainer/app.py
+++ backend-trainer/app.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 import socketio
-from models import dbname, projectsModel
+from models import dbname, projectsModel, domainsModel
 
 # creates a new Async Socket IO Server
 
@@ -36,18 +36,36 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 
+# Rooms Hooks
+
+@sio.on('join_room')
+async def join_room(sid, room_name):
+    print(" User {} joined room {}".format(sid,room_name))
+    sio.enter_room(sid, room_name)
+
+
+@sio.on('leave_room')
+async def leave_room(sid, room_name):
+    print(" User {} left room {}".format(sid, room_name))
+    sio.leave_room(sid, room_name)
+
+
 # projects Endpoints
 
 
 @sio.on('getProjects', namespace='/project')
 async def getProjects(sid):
-    result= await projectsModel.getProjects()
-    print("Got result {} from session {}".format(result, sid))
+
+    print("---------- Request from Session {} -------------- ".format(sid))
+
+    result = await projectsModel.getProjects()
     await sio.emit('allProjects', result, namespace='/project')
 
 
 @sio.on('createProject', namespace='/project')
 async def createProjects(sid, record):
+
+    print("---------- Request from Session {} -------------- ".format(sid))
 
     record_id= await projectsModel.createProjects(record)
     await sio.emit('projectResponse', {"message": "Project created with ID {} ".format(record_id)}, namespace='/project', room=sid)
@@ -59,6 +77,8 @@ async def createProjects(sid, record):
 @sio.on('deleteProject', namespace='/project')
 async def deleteProject(sid, object_id):
 
+    print("---------- Request from Session {} -------------- ".format(sid))
+
     record_id = await projectsModel.deleteProject(object_id)
     await sio.emit('projectsResponse', {'message': 'project Deleted with id {}'.format(record_id)}, namespace='/project', room=sid)
 
@@ -69,7 +89,7 @@ async def deleteProject(sid, object_id):
 @sio.on('updateProject', namespace='/project')
 async def updateProject(sid, update_query):
 
-    update_query = {"object_id": "123", "project_id": "123", "project_name": "project_name", "project_description" : "project description"}
+    print("---------- Request from Session {} - with request {} ------- ".format(sid, update_query))
 
     result = await projectsModel.updateProject(update_query)
     await sio.emit('projectsResponse', {"message": "Updated project with row {}".format(result)}, namespace='/project', room=sid)
@@ -82,22 +102,34 @@ async def updateProject(sid, update_query):
 # Domains Endpoints
 
 
-@sio.on('getDomain', namespace='/domain')
-async def get_projects(sid):
-    print(sid)
-    print("Got request on Get domain Resource")
-    await sio.emit('domainResponse', {'message': 'domain Details'})
+@sio.on('getDomains', namespace='/domain')
+async def getDomains(sid, project_id):
+
+    print("---------- Request from Session {} -------------- ".format(sid))
+
+    result = await domainsModel.getDomains(project_id)
+    await sio.emit('allDomains', result, namespace='/domain', room=project_id)
 
 
 @sio.on('createDomain', namespace='/domain')
-async def get_projects(sid):
-    print(sid)
-    print("Got request on create domian Resource")
-    await sio.emit('domainResponse', {'message': 'domain ID created'})
+async def createDomains(sid, data):
+
+    print("---------- Request from Session {} -------------- ".format(sid))
+
+    data = {"project_id": "1", "domain_id":"1", "domain_name":"New domain", "domain_description":"Test domain"}
+    result = await domainsModel.createDomain(data)
+    await sio.emit('domainResponse', {'message': 'Domain created with ID {}'.format(result)}, namespace='/domain', room=sid)
 
 
 @sio.on('deleteDomain', namespace='/domain')
-async def get_projects(sid):
+async def deleteDomains(sid):
+    print(sid)
+    print("Got request on Delete domain Resource")
+    await sio.emit('domainResponse', {'message': 'domain Deleted '})
+
+
+@sio.on('updateDomain', namespace='/domain')
+async def updateDomains(sid):
     print(sid)
     print("Got request on Delete domain Resource")
     await sio.emit('domainResponse', {'message': 'domain Deleted '})
