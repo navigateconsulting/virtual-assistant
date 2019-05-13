62f9106cbff319e374fff39ae0d50f23e9860946
sai <sai.kiran@navigateconsulting.in>
2019-05-13 23:16:37 +0530

    Endpoints - to be used in Future to move all end point code int his file -

--- backend-trainer/endpoints.py
+++ backend-trainer/endpoints.py
@@ -1,13 +1,4 @@
-from . import app
-from . import socketio
+import os
 
-@socketio.on('getEntities', namespace='/entity')
-async def getEntities(sid, data):
 
-    print("---------- Request from Session {} -- with record {} -- and room {} ----------  ".format(sid, data, room_name))
-
-    data = {"project_id": "123"}
-
-    entities_list = entityModel.getEntities(data)
-
-    await sio.emit('allEntities', entities_list, namespace='/entity')
+## Stub
