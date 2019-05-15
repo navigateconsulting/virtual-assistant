e70beb809aeab856a2f58361cb783b25ee988cb8
Nishant Shuke <nishant1712>
2019-05-15 14:32:07 +0530

    Polyfills Change

--- ui-trainer/src/polyfills.ts
+++ ui-trainer/src/polyfills.ts
@@ -83,3 +83,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any)['global'] = window;
