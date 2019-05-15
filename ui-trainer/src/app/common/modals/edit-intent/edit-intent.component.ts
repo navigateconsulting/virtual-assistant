3c5114036f807c2e32a70faaebff46925024d8be
Nishant Shuke <nishant1712>
2019-05-15 18:49:32 +0530

    Intents Done & Responses UI Done

---
+++ ui-trainer/src/app/common/modals/edit-intent/edit-intent.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-intent',
+  templateUrl: './edit-intent.component.html',
+  styleUrls: ['./edit-intent.component.scss']
+})
+export class EditIntentComponent implements OnInit {
+
+  editIntentForm: FormGroup;
+
+  constructor(public dialogRef: MatDialogRef<EditIntentComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+    this.editIntentForm = new FormGroup({
+      intentName: new FormControl(this.data.intentName, Validators.required),
+      intentDescription: new FormControl(this.data.intentDescription, Validators.required)
+    });
+  }
+
+  closeDialog() {
+    if (this.editIntentForm.valid) {
+      this.dialogRef.close({
+        project_id: this.data.projectObjectId,
+        domain_id: this.data.domainObjectId,
+        object_id: this.data.intentObjectId,
+        intent_name: this.editIntentForm.value.intentName,
+        intent_description: this.editIntentForm.value.intentDescription
+      });
+    }
+  }
+
+}
