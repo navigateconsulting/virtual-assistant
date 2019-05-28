da059d955eb713d38b378b537eccd1cdd87070af
sai <sai.kiran@navigateconsulting.in>
2019-05-28 12:14:00 +0530

    setting global var's to reset values during second training attempt

--- backend-trainer/export_project.py
+++ backend-trainer/export_project.py
@@ -17,6 +17,20 @@ class ExportProject:
         self.project_home = ''
         self.project_base_path = '../vol_chatbot_data/temp/trainer-sessions/'
         self.session_id = ''
+        self.master_nlu = {"rasa_nlu_data": {"common_examples": []}}
+        self.master_stories = ""
+        self.master_domain_intents = ""
+        self.master_domain_actions = ""
+        self.master_domain_templates = ""
+
+    async def reset_globals(self, sid):
+        self.master_nlu = {"rasa_nlu_data": {"common_examples": []}}
+        self.master_stories = ""
+        self.master_domain_intents = ""
+        self.master_domain_actions = ""
+        self.master_domain_templates = ""
+        self.session_id = sid
+        self.project_home = self.project_base_path + self.session_id
 
     async def clean_up(self, sid):
         print("Cleaning up on session disconnect for session ID {}".format(sid))
@@ -29,21 +43,18 @@ class ExportProject:
         # Invoke Method to start parallel activity
         print("Starting export for project  ID {}".format(project_id))
 
+        await self.reset_globals(sid)
+
         if os.path.isdir(self.project_base_path+sid):
             print("Directory already exists , Empty the directory")
             print("Working on project home {}".format(self.project_base_path + sid))
-            self.session_id = sid
-            self.project_home = self.project_base_path+self.session_id
             shutil.rmtree(self.project_base_path+sid)
-            os.mkdir(self.project_home)
 
         else:
             print("Working on project home {}".format(self.project_base_path + sid))
 
-            self.session_id = sid
-            self.project_home = self.project_base_path + self.session_id
-            os.mkdir(self.project_home)
-
+        os.mkdir(self.project_home)
+        os.mkdir(self.project_home + '/data')
         result = await self.start_export(project_id)
         print(result)
 
@@ -68,6 +79,45 @@ class ExportProject:
         result = await asyncio.gather(self.export_nlu_data(project_id, domain_id, domain_name),
                                       self.export_domain_yml_data(project_id, domain_id, domain_name),
                                       self.export_stories(project_id, domain_id, domain_name))
+
+        # Write master consolidated NLU file to disk
+        print("Writing Consolidated files to Disk")
+
+        async with aiofiles.open(self.project_home + '/data/nlu.json', "w") as out:
+            await out.write(json.dumps(self.master_nlu, indent=4, sort_keys=False))
+            await out.flush()
+
+        async with aiofiles.open(self.project_home + '/data/stories.md', "w") as out:
+            await out.write(self.master_stories)
+            await out.flush()
+
+        async with aiofiles.open(self.project_home + '/domain.yml', "w") as out:
+            await out.write("intents:"+"\n")
+            await out.write(self.master_domain_intents + "\n" + "\n")
+
+            await out.write("actions:"+"\n")
+            await out.write(self.master_domain_actions + "\n" + "\n")
+
+            await out.write("templates:" + "\n")
+            await out.write(self.master_domain_templates + "\n" + "\n")
+
+            await out.flush()
+
+        print("Writing Default config file to Disk")
+
+        async with aiofiles.open(self.project_home + '/config.yml', "w") as out:
+
+            await out.write("# Configuration for Rasa NLU." + "\n\n")
+            await out.write("language: en" + "\n")
+            await out.write("pipeline: supervised_embeddings" + "\n")
+
+            await out.write("# Configuration for Rasa Core." + "\n\n")
+            await out.write("policies:" + "\n")
+            await out.write("  - name: MemoizationPolicy" + "\n")
+            await out.write("  - name: KerasPolicy" + "\n")
+            await out.write("  - name: MappingPolicy" + "\n")
+            await out.flush()
+
         return result
 
     async def export_nlu_data(self, project_id, domain_id, domain_name):
@@ -83,6 +133,7 @@ class ExportProject:
                                "intent": intents['intent_name'],
                                "entities": intent['entities']}
                 base_nlu_stub['rasa_nlu_data']['common_examples'].append(json_record)
+                self.master_nlu['rasa_nlu_data']['common_examples'].append(json_record)
 
         async with aiofiles.open(self.project_home + '/skills/' + domain_name + '/data/nlu.json', "w") as out:
             await out.write(json.dumps(base_nlu_stub, indent=4, sort_keys=False))
@@ -96,14 +147,18 @@ class ExportProject:
             for stories in stories_list:
                 story_detail = await self.StoryModel.get_only_story_details({'object_id': stories['_id']['$oid']})
                 await out.write("##"+stories['story_name']+"\n")
+                self.master_stories = self.master_stories + "##"+stories['story_name']+"\n"
                 for story_rec in story_detail['story']:
                     if story_rec['type'] == 'intent':
                         await out.write("* "+story_rec['key']+"\n")
+                        self.master_stories = self.master_stories + "* "+story_rec['key']+"\n"
                     else:
                         await out.write("  - "+story_rec['key']+"\n")
+                        self.master_stories = self.master_stories + "  - "+story_rec['key']+"\n"
 
                 await out.write("\n")
                 await out.write("\n")
+                self.master_stories = self.master_stories + "\n"+"\n"
             await out.flush()
 
     async def export_domain_yml_data(self, project_id, domain_id, domain_name):
@@ -115,26 +170,32 @@ class ExportProject:
 
         async with aiofiles.open(self.project_home + '/skills/' + domain_name + '/domain.yml', "w") as out:
             await out.write("intents:"+"\n")
+            #self.master_domain_intents = self.master_domain_intents + "intents:"+"\n"
             for intents in intents_list:
                 await out.write("- "+intents['intent_name']+"\n")
+                self.master_domain_intents = self.master_domain_intents + "- "+intents['intent_name']+"\n"
             await out.write("\n")
             await out.write("\n")
+            #self.master_domain_intents = self.master_domain_intents + "\n" + "\n"
 
             response_list = await self.ResponseModel.get_responses({"project_id": project_id, "domain_id": domain_id})
 
             await out.write("actions:"+"\n")
             for resp in response_list:
                 await out.write("- "+resp['response_name']+"\n")
+                self.master_domain_actions = self.master_domain_actions + "- "+resp['response_name']+"\n"
             await out.write("\n")
             await out.write("\n")
 
             await out.write("templates:" + "\n")
             for response in response_list:
                 await out.write("  "+response['response_name']+":\n")
+                self.master_domain_templates = self.master_domain_templates + "  "+response['response_name']+":\n"
                 response_detail = await self.ResponseModel.get_response_details({"object_id": response['_id']['$oid']})
                 for resp_value in response_detail['text_entities']:
                     await out.write("  - "+"text: "+f'"'+resp_value+f'"')
                     await out.write("\n")
+                    self.master_domain_templates = self.master_domain_templates + "  - "+"text: "+f'"'+resp_value+f'"'+"\n"
                 await out.write("\n")
             await out.write("\n")
             await out.flush()
