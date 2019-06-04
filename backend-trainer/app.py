3f1b6d12f77a9bde9dd751dd2b6e8692857ba424
sai <sai.kiran@navigateconsulting.in>
2019-06-04 17:24:21 +0530

    made changes to import config and dockerbuild

--- backend-trainer/app.py
+++ backend-trainer/app.py
@@ -1,7 +1,14 @@
 from aiohttp import web
 import socketio
+from config import CONFIG
 
-sio = socketio.AsyncServer(logger=True, engineio_logger=True)
+
+if CONFIG.get('backend-trainer', 'LOGGING') == 'TRUE':
+    print("--------------------Starting Socketio Connection in Debug Mode --------------------------")
+    sio = socketio.AsyncServer(logger=True, engineio_logger=True)
+else:
+    sio = socketio.AsyncServer(logger=False, engineio_logger=False)
+
 app = web.Application()
 sio.attach(app)
 
@@ -11,8 +18,8 @@ async def index(request):
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
 
-# Imports for Endpoints
 
+# Imports for Endpoints
 
 import endpoints
 import rooms_endpoints
@@ -22,4 +29,4 @@ app.router.add_get('/', index)
 # We kick off our server
 if __name__ == '__main__':
     sio.attach(app)
-    web.run_app(app, port=8089)
+    web.run_app(app, port=CONFIG.get('backend-trainer', 'PORT'))
