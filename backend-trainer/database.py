29d05f3c4333fde17791843bdfccf55a1732605e
sai <sai.kiran@navigateconsulting.in>
2019-06-04 17:23:35 +0530

    made changes to import config and dockerbuild

--- backend-trainer/database.py
+++ backend-trainer/database.py
@@ -1,10 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from config import CONFIG
 
 
 # noinspection PyMethodMayBeStatic
 class ConDatabase:
 
     def connect():
-        client = AsyncIOMotorClient('mongodb://localhost:27017')
-        db = client['eva_platform']
+        client = AsyncIOMotorClient(CONFIG.get('backend-trainer', 'MONGODB_URL'))
+        db = client[CONFIG.get('backend-trainer', 'MONGODB_NAME')]
         return db
