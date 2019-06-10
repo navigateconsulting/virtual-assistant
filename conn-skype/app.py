2bfcc1817cb1551cf49987f96216eb7437812dc1
sai <sai.kiran@navigateconsulting.in>
2019-06-10 17:05:01 +0530

    new skype endpoint

--- conn-skype/app.py
+++ conn-skype/app.py
@@ -8,10 +8,12 @@ from skype_bot import SkypeBot
 
 async def main():
     bot = SkypeBot()
-    await bot.setup_ucwa("http://lyncdiscover.navigateconsulting.in",
-                         "daas@navigateconsulting.in",
-                         "Cfxdzs@123",
-                         "830b6148-30da-4b86-a621-e7d724241f7a")
+    await bot.setup_ucwa(discover_url="http://lyncdiscover.navigateconsulting.in",
+                         username="daas@navigateconsulting.in",
+                         password="Cfxdzs@123",
+                         client_id="830b6148-30da-4b86-a621-e7d724241f7a",
+                         tenant_id='2f892c02-6a40-43d0-840d-f2485c93fd06'
+                         )
     await bot.close_session()
 
 asyncio.run(main())
--- conn-skype/skype_bot.py
+++ conn-skype/skype_bot.py
@@ -1,24 +1,44 @@
 from __main__ import aiohttp
 import re
-import msal
+import adal
+import json
 
 
 class SkypeBot:
 
     def __init__(self):
         print("inside init ")
-        self.dummy = "Value"
         self.user_url = None
         self.discover_url = None
         self.username = None
         self.password = None
         self.client_id = None
+        self.tenant_id = None
+        self.domain = None
+        self.user_token = None
+        self.app_token = None
+
+        # Communication Variables
+        self.meTasks = None
+        self.peopleTasks = None
+        self.meetingTasks = None
+        self.communicationTasks = None
+        self.eventsURL = None
+        self.reportMyActivity = None
+        self.applicationURL = None
+        self.hub_address = None
+        self.events_timeout = "&timeout=90"
+
+        self.post_headers = None
+        self.get_headers = None
+
+        self.matchString = 'https?:\/\/(?:[^\/]+)|^(.*)$'
         self._session = aiohttp.ClientSession()
 
     async def close_session(self):
         await self._session.close()
 
-    async def setup_ucwa(self, discover_url, username, password, client_id):
+    async def setup_ucwa(self, discover_url, username, password, client_id, tenant_id):
         print("__________________  Starting connection to UCWA  ___________________________")
 
         async with self._session.get(discover_url) as res:
@@ -36,48 +56,207 @@ class SkypeBot:
         self.username = username
         self.password = password
         self.client_id = client_id
+        self.tenant_id = tenant_id
+        self.domain = self.username[self.username.index("@")+1:]
 
-        await self.authorise()
+        await self.init_application()
 
-    async def authorise(self):
+        # Test Calls
 
-        print("Authenticating on the user URL ")
+        await self.report_my_activity()
 
-        async with self._session.get(self.user_url) as res:
-            print(res)
+        await self.set_my_presence('Online')
+        result = await self.get_my_presence()
+        print("Presence {}".format(result))
 
-        search_res = re.search('MsRtcOAuth href="(.*?)"', str(res))
-        resource = search_res.group(1)
-        #resource = resource.split("//")[-1].split("/")[0].split('?')[0]
-        print("Resource {}".format(resource))
+        await self.set_note("Welcome to Eva")
+        result = await self.get_note()
+        print("Note value {}".format(result))
 
-        search_auth_uri = re.search('authorization_uri="(.*?)"', str(res))
-        auth_endpoint_url = search_auth_uri.group(1)
-        print(auth_endpoint_url)
+        await self.set_location("Pune")
+        result = await self.get_location()
+        print("Location {}".format(result))
 
-        app = msal.PublicClientApplication(client_id=self.client_id, authority="https://login.microsoftonline.com/navigateconsulting.in")
+    async def get_adal_token(self, url):
 
-        result = app.acquire_token_by_username_password(username=self.username, password=self.password, scopes=["https://graph.microsoft.com/.default"])
+        # get hub URL
+        hub_address = re.search(self.matchString, url).group(0)
 
-        if "access_token" in result:
-            print(result["access_token"])
-            print(result["token_type"])
-            print(result["expires_in"])  # You don't normally need to care about this.
-            # It will be good for at least 5 minutes.
+        # get Access token using tenant specific login URL
+        context = adal.AuthenticationContext(
+            'https://login.microsoftonline.com/' + self.tenant_id, validate_authority=self.domain
+        )
+
+        token = context.acquire_token_with_username_password(
+            hub_address,
+            self.username,
+            self.password,
+            self.client_id
+        )
+        return 'Bearer ' + token['accessToken']
+
+    async def make_me_available(self, application_url, me_tasks):
+
+        hub_address = re.search(self.matchString, application_url).group(0)
+
+        if 'makeMeAvailable' in me_tasks:
+            url = hub_address+me_tasks['makeMeAvailable']['href']
+            data = {'signInAs': 'Online',
+                    'SupportedModalities': ['Messaging'],
+                    'supportedMessageFormats' : ['Plain', 'Html']}
+            header={
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': self.app_token
+                }
+            async with self._session.post(url=url, data=json.dumps(data), headers=header) as res:
+                print(res.status)
+                json_res = res
+                # TODO Need to check what is the status when the user is offline and handle accordingly
         else:
-            print(result.get("error"))
-            print(result.get("error_description"))
-            print(result.get("correlation_id"))  # You may need this when reporting a bug
-            if 65001 in result.get("error_codes", []):  # Not mean to be coded programatically, but...
-                # AAD requires user consent for U/P flow
-                print("Visit this to consent:", app.get_authorization_request_url(scope))
+            return 'NA'
+        return json_res
 
-        header_val = {"Authorization" : "Bearer "+result["access_token"],
-                      "Accept" : "application/json",
-                      "X-Requested-With": "XMLHttpRequest"
-                    }
+    async def init_application(self):
 
-        async with self._session.get(self.user_url,
-                                     headers=header_val) as res:
-            print(res)
+        # Step 1 - get Access token for User URL and perform a get on User URL , this gives us Application URL
 
+        # Get User Access Token and get application resource via USER token
+
+        self.user_token = await self.get_adal_token(self.user_url)
+        print("User Token {}".format(self.user_token))
+
+        header_val = { "Authorization" : self.user_token,
+                       "Accept" : "application/json",
+                       "X-Requested-With": "XMLHttpRequest",
+                       #"Referer": user_xframe,
+                       "Accept-Encoding" : "gzip, deflate",
+                       "User-Agent" : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+                       "Host": self.domain,
+                       "Connection": "Keep-Alive"
+                        }
+
+        async with self._session.get(self.user_url, headers=header_val) as res:
+            json_resp = await res.json()
+
+        applications_url = json_resp['_links']['applications']['href']
+
+        # Step 2 - From the applications URL Obtained , get a access token for the applications URL , and perform a POST on application
+        # URL to get resources within that application Here we use Application token and obtain Skype Resources
+
+        # Getting application token and authenticating to the application resource to get further resources
+        self.app_token = await self.get_adal_token(applications_url)
+        print(self.app_token)
+
+        post_header_val = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": self.app_token,
+            "Accept-Language": "en-us",
+            "Accept-Encoding": "gzip, deflate",
+            "User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+            ##"Host": "webpoolhkn0f03.infra.lync.com",  ## no need to pass host value but this is good to have
+            "Connection": "Keep-Alive",
+            "Cache-Control": "no-cache"
+        }
+
+        # TODO User agent and Endpoint ID should be set Dynamically
+        post_body_val = {
+            'UserAgent': 'Eva Chatbot',
+            'Culture': 'en-US',
+            'EndpointId': 'xxxxxxxx-xxxx-8xxx-yxxx-xxxxxxxxxxxx'
+        }
+
+        async with self._session.post(applications_url, data=json.dumps(post_body_val), headers=post_header_val) as res:
+            json_resp = await res.json()
+
+        # Step 3 Initial Post on application URL would not give all endpoints , Hence we need to make the user available
+        # and then post again on application URL to get all endpoints
+
+        result = await self.make_me_available(applications_url, json_resp['_embedded']['me']['_links'])
+        print("Making BOT Available {}".format(result))
+
+        # Step 4 Performing POST on application resource again to get all Endpoints set.
+
+        async with self._session.post(applications_url, data=json.dumps(post_body_val), headers=post_header_val) as res:
+            json_resp = await res.json()
+
+        # Finally all resources are available setting variables to there values
+
+        self.meTasks = json_resp['_embedded']['me']['_links']
+        self.peopleTasks = json_resp['_embedded']['people']['_links']
+        self.meetingTasks = json_resp['_embedded']['onlineMeetings']['_links']
+        self.communicationTasks = json_resp['_embedded']['communication']['_links']
+        self.eventsURL = json_resp["_links"]["events"]["href"]
+        self.reportMyActivity = self.meTasks['reportMyActivity']['href']
+        self.applicationURL = json_resp['_links']['self']['href']
+        self.hub_address = re.search(self.matchString, applications_url).group(0)
+
+        # Setting generic Headers
+
+        self.post_headers = {'Accept': 'application/json',
+                             'Content-Type': 'application/json',
+                             'Authorization': self.app_token
+                             }
+
+        self.get_headers = {'Accept': "application/json",
+                            'Connection': "keep-alive",
+                            'Accept-Encoding': "gzip, deflate",
+                            'Authorization': self.app_token
+                            }
+
+        print("ApplicationURL var {}".format(self.applicationURL))
+        print("Hub Address var {}".format(self.hub_address))
+
+    async def terminate_application(self):
+
+        async with self._session.delete(url=self.hub_address+self.applicationURL, headers=self.post_headers) as res:
+            json_res = await res.status
+        print("Terminating Application Status {}".format(json_res))
+
+    async def report_my_activity(self):
+
+        async with self._session.post(url=self.hub_address+self.reportMyActivity, headers=self.post_headers) as res:
+            print("Reporting Activity Status {}".format(res.status))
+
+    async def set_my_presence(self, value):
+
+        values = ['Away', 'BeRightBack', 'Busy', 'DoNotDisturb', 'Offwork', 'Online']
+        if value in values:
+            data = {"availability": value}
+            async with self._session.post(url=self.hub_address+self.meTasks['presence']['href'], data=json.dumps(data)) as res:
+                print("Setting presence status {}".format(res.status))
+            print(res.status)
+        else:
+            raise KeyError("401 Invalid value")
+
+    async def get_my_presence(self):
+
+        async with self._session.get(url=self.hub_address+self.meTasks['presence']['href'], headers=self.get_headers) as res:
+            json_res = await res.json()
+        return json_res['availability']
+
+    async def set_note(self, value):
+
+        data = {"message": value}
+        async with self._session.post(url=self.hub_address+self.meTasks['note']['href'], data=json.dumps(data), headers=self.post_headers) as res:
+            print("Setting Note status {}".format(res.status))
+
+    async def get_note(self):
+
+        async with self._session.get(url=self.hub_address+self.meTasks['note']['href'], headers=self.get_headers) as res:
+            json_res = await res.json()
+
+        return json_res['message']
+
+    async def set_location(self, location):
+
+        data = {"location": location}
+
+        async with self._session.post(url=self.hub_address+self.meTasks['location']['href'], data=json.dumps(data), headers=self.post_headers) as res:
+            print("Location updated status {}".format(res.status))
+
+    async def get_location(self):
+        async with self._session.get(url=self.hub_address+self.meTasks['location']['href'], headers=self.get_headers) as res:
+            json_res = await res.json()
+        return json_res['location']
