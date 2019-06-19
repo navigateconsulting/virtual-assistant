9bfd1d719399978afe90cd3dca83a10e78ce0d63
Nishant Shuke <nishant1712>
2019-06-19 20:00:49 +0530

    First UX Commit: Projects Page Done

--- ui-trainer/src/app/app.component.ts
+++ ui-trainer/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eva-chatbot-app';
+
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(
+      'project',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/project.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'edit',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/edit.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'delete',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/delete.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'copy',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/copy.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'try-now',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/try-now.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'domain',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/domain.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'intent',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/intent.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'response',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/response.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'story',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/images/story.svg')
+    );
+  }
 }
