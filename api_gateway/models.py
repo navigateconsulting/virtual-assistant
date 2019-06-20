419828a2ba2a8d08a6fdeb3efdef4715ee61deca
sai <sai.kiran@navigateconsulting.in>
2019-06-20 14:35:33 +0530

    Added deault system built in actions and modified Refresh data code to import actions from seed data

--- api_gateway/models.py
+++ api_gateway/models.py
@@ -39,6 +39,7 @@ class RefreshDb:
         await db.responses.delete_many({})
         await db.stories.delete_many({})
         await db.conversations.delete_many({})
+        await db.actions.delete_many({})
 
         # Inserting Data in collection
 
@@ -82,6 +83,10 @@ class RefreshDb:
             await db.stories.insert_many(data)
 
         await db.stories.update_many({}, {'$set': {'project_id': str(project_id), 'domain_id': str(domain_id.get('_id'))}})
+
+        with open(seed_data_path+'actions.json') as json_file:
+            data = json.load(json_file)
+            await db.actions.insert_many(data)
 
         return "Success"
 
