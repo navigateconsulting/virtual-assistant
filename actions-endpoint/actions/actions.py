a297fa23d353fabf67a6a3d2bed0652129beeff5
sai <sai.kiran@navigateconsulting.in>
2019-09-04 14:44:24 +0530

    bug fixes , Oracle db connector

--- actions-endpoint/actions/actions.py
+++ actions-endpoint/actions/actions.py
@@ -12,6 +12,30 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+import cx_Oracle
+
+# Connect to oracle database
+connection = cx_Oracle.connect("apps", "apps", "vision.ncbs.com/VIS")
+
+
+class ActionOnhandQuantity(Action):
+
+    def name(self) -> Text:
+        return "action_onhand_quantity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        cursor = connection.cursor()
+        cursor.execute("select transaction_id from mtl_material_transactions where rownum = 1")
+
+        for transaction_id in cursor:
+            dispatcher.utter_message("Transaction ID is {}".format(transaction_id))
+
+        #dispatcher.utter_message("My first action ")
+
+        return []
+
 
 class ActionHelloWorld(Action):
 
