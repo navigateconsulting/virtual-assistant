023072931ebcdbabfa16fb3f21a233389d2d50ec
sai <sai.kiran@navigateconsulting.in>
2019-10-01 12:31:48 +0530

    updated template actions file

--- vol_chatbot_data/rasa/server/actions/actions.py
+++ vol_chatbot_data/rasa/server/actions/actions.py
@@ -1,17 +1,14 @@
+import requests
+import json
 from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
 
-class ActionCheckRestaurants(Action):
-   def name(self) -> Text:
-      return "action_check_restaurants"
 
-   def run(self,
-           dispatcher: CollectingDispatcher,
-           tracker: Tracker,
-           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+class ActionJoke(Action):
+    def name(self):
+        return "action_joke"
 
-      cuisine = tracker.get_slot('cuisine')
-      q = "select * from restaurants where cuisine='{0}' limit 1".format(cuisine)
-      result = db.query(q)
-
-      return [SlotSet("matches", result if result is not None else [])]
+    def run(self, dispatcher, tracker, domain):
+        request = requests.get('http://api.icndb.com/jokes/random').json()  # make an api call
+        joke = request['value']['joke']  # extract a joke from returned json response
+        dispatcher.utter_message(joke)  # send the message back to the user
+        return []
