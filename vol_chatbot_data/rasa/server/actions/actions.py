fcb8bda86498e962e7dd2d8a5f40fb18bd9ee88f
Nishant Shuke <nishantshuke@Nishants-MacBook-Pro.local>
2020-04-22 19:02:58 +0530

    Changes in Action API

--- vol_chatbot_data/rasa/server/actions/actions.py
+++ vol_chatbot_data/rasa/server/actions/actions.py
@@ -26,9 +26,10 @@ class ActionLogServiceRequest(Action):
         url = 'http://esas_api_gateway:5000/api/v1/incident'
         user_id = tracker.sender_id
         service_group = "Queue"
+        all_slots = tracker.current_slot_values()
         issue_details = tracker.get_slot('issue_details')
         headers = {'content-type': 'application/json'}
-        insert_record = {"created_by": user_id, "request_summary": issue_details, "request_description": issue_details, "assign_group_name": service_group, "status": '1', "priority": '1', "assignee": 'null', "resolution": "", "last_assigned_by": 'null'}
+        insert_record = {"created_by": user_id, "request_summary": issue_details, "request_description": issue_details, "assign_group_name": service_group, "status": '1', "priority": '1', "assignee": 'null', "resolution": "", "last_assigned_by": 'null', "slot_values": json.dumps(all_slots)}
         x = requests.post(url, data=json.dumps(insert_record), headers=headers)
         logging.warning(x)
         if x.status_code == 200:
